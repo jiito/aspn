a3de720337564d8dd3c26755e828b22848c0b2f7
jiito <benjamin.allanrahill@gmail.com>
2022-10-23 21:05:49 -0400

    feat: creating developers table

--- pando/src/models.rs
+++ pando/src/models.rs
@@ -1,7 +1,7 @@
-use crate::schema::projects;
+use crate::schema::*;
 use diesel::prelude::*;
 
-#[derive(Queryable)]
+#[derive(Queryable, Identifiable)]
 pub struct Project {
     pub id: i32,
     pub name: String,
@@ -11,4 +11,14 @@ pub struct Project {
 #[diesel(table_name = projects)]
 pub struct NewProject<'a> {
     pub name: &'a str,
+}
+
+#[derive(Queryable, Associations, Identifiable)]
+#[belongs_to(Project)]
+#[table_name = "developers"]
+pub struct Developer {
+    pub id: i32,
+    pub name: String,
+    pub ip_address: String,
+    pub project_id: i32,
 }
--- pando/src/schema.rs
+++ pando/src/schema.rs
@@ -1,8 +1,24 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    developers (id) {
+        id -> Int4,
+        name -> Varchar,
+        ip_address -> Inet,
+        project_id -> Int4,
+    }
+}
+
+diesel::table! {
     projects (id) {
         id -> Int4,
         name -> Varchar,
     }
 }
+
+diesel::joinable!(developers -> projects (project_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    developers,
+    projects,
+);
