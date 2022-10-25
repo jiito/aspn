6c71d4d60b9c69283cd1ddd3fb4b65988872cd45
Benjamin Allan-Rahill <benjamin.allanrahill@gmail.com>
2022-10-24 23:12:52 -0400

    Asp 39 connect to cloudsql from rust (#2)
    * feat: working code to create new project
    
    * feat: creating developers table

--- pando/src/lib.rs
+++ pando/src/lib.rs
@@ -4,6 +4,7 @@ pub mod schema;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use dotenvy::dotenv;
+use models::{NewProject, Project};
 use std::env;
 
 #[cfg(test)]
@@ -20,4 +21,15 @@ pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_project(conn: &mut PgConnection, name: &str) -> Project {
+    use crate::schema::projects;
+
+    let new_project = NewProject { name };
+
+    diesel::insert_into(projects::table)
+        .values(&new_project)
+        .get_result(conn)
+        .expect("Error saving project")
 }
--- pando/src/models.rs
+++ pando/src/models.rs
@@ -1,7 +1,24 @@
+use crate::schema::*;
 use diesel::prelude::*;
 
-#[derive(Queryable)]
+#[derive(Queryable, Identifiable)]
 pub struct Project {
     pub id: i32,
     pub name: String,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = projects)]
+pub struct NewProject<'a> {
+    pub name: &'a str,
+}
+
+#[derive(Queryable, Associations, Identifiable)]
+#[belongs_to(Project)]
+#[table_name = "developers"]
+pub struct Developer {
+    pub id: i32,
+    pub name: String,
+    pub ip_address: String,
+    pub project_id: i32,
 }
