32aae7ab7f42aae24958c577b8010d127ceaf856
jiito <benjamin.allanrahill@gmail.com>
2022-10-26 16:59:30 -0400

    feat: make test request

--- pando/src/storage/gcs.rs
+++ pando/src/storage/gcs.rs
@@ -1,7 +1,20 @@
-pub fn upload_file() {
-    println!("Uploading a file!")
+pub async fn upload_file() {
+    let res = request_gcp().await;
+
+    println!("res = {:#?}", res)
 }
 
 pub fn download_file() {
     println!("Downloading file")
+}
+
+async fn request_gcp() -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let body = client
+        .get("https://example.com")
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(body)
 }
