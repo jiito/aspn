47ae46fe73a8cd94189cf8e7653d3c77cb54175b
jiito <benjamin.allanrahill@gmail.com>
2022-10-27 08:50:47 -0400

    refactor: move parameter hierarchy up a function

--- pando/src/storage/gcs.rs
+++ pando/src/storage/gcs.rs
@@ -1,28 +1,23 @@
-use std::fs;
-
-use reqwest::{header, Body};
+use reqwest::{
+    header::{self, HeaderMap},
+    Body,
+};
 use tokio::fs::File;
 
-pub async fn upload_file() {
-    let res = request_gcp().await;
-
-    println!("res = {:#?}", res)
-}
-
-pub fn download_file() {
-    println!("Downloading file")
-}
-
-async fn request_gcp() -> Result<String, Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
-    let file_location = "/Users/bjar/foo.txt";
-    let oauth_token = "ya29.a0Aa4xrXP6IhG3rWKi-5uAbykp_tbTBgWTifE-MQREPsrN6pS45C_QRy6uRWwadK7DvkdHvJDl89hmTFq5yThdFob-wLvMDyc2m2oxr7ypjxYZgoo_mQJg7ngXcMFTo6_4JpgXrpCpK4p8m27gc5LkikYQaPbXaCgYKATASARISFQEjDvL9sFegKq_LX08Zg4NpvZqrjQ0163";
+pub async fn upload_file(
+    file_path: String,
+    oauth_token: String,
+    object_name: String,
+    bucket_name: String,
+) {
+    let file_location = String::from("/Users/bjar/foo.txt");
+    let oauth_token = String::from("ya29.a0Aa4xrXP6IhG3rWKi-5uAbykp_tbTBgWTifE-MQREPsrN6pS45C_QRy6uRWwadK7DvkdHvJDl89hmTFq5yThdFob-wLvMDyc2m2oxr7ypjxYZgoo_mQJg7ngXcMFTo6_4JpgXrpCpK4p8m27gc5LkikYQaPbXaCgYKATASARISFQEjDvL9sFegKq_LX08Zg4NpvZqrjQ0163");
     let object_content_type = "text/plain";
     let object_name = "testObject";
     let bucket_name = "aspn_functions";
 
     // open file
-    let file = File::open(file_location).await?;
+    let file = File::open(file_location).await.unwrap();
     let body = Body::from(file);
 
     let mut headers = header::HeaderMap::new();
@@ -33,16 +28,32 @@ async fn request_gcp() -> Result<String, Box<dyn std::error::Error>> {
 
     let uri = format!("https://storage.googleapis.com/upload/storage/v1/b/{bucket_name}/o?uploadType=media&name={object_name}");
 
+    let res = request_gcp(uri, headers, body, oauth_token).await;
+
+    println!("res = {:#?}", res)
+}
+
+pub fn download_file() {
+    println!("Downloading file")
+}
+
+async fn request_gcp(
+    path: String,
+    headers: HeaderMap,
+    body: Body,
+    oauth_token: String,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+
     let response = client
-        .post(uri)
+        .post(path)
         .headers(headers)
         .bearer_auth(oauth_token)
         .body(body)
         .send()
+        .await?
+        .json()
         .await?;
 
-    println!("Status: {}", response.status());
-    println!("Status: {:#?}", response.text().await?);
-
-    Ok(String::new())
+    Ok(response)
 }
