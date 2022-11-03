e6c7a752c5f157f7e49a91854750b155a7b6d6d2
jiito <benjamin.allanrahill@gmail.com>
2022-11-03 09:40:27 -0400

    [asp-53]: feat: generate signed url for method and object

--- pando/src/api/handlers.rs
+++ pando/src/api/handlers.rs
@@ -3,6 +3,8 @@ use std::io::Error;
 use actix_web::{web, HttpResponse};
 use serde::Deserialize;
 
+use crate::storage;
+
 #[derive(Deserialize)]
 pub struct SignedUrlQueryParams {
     method: String,
@@ -12,8 +14,16 @@ pub struct SignedUrlQueryParams {
 pub async fn signed_url_handler(
     query_params: web::Query<SignedUrlQueryParams>,
 ) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json(format!(
-        "{}, {}",
-        query_params.method, query_params.object_name
-    )))
+    // build signed url
+
+    let uri = storage::gcs::generate_signed_url(
+        "/Users/bjar/service-account.json".into(),
+        "aspn_functions".into(),
+        query_params.object_name.clone(),
+        None,
+        query_params.method.clone(),
+        None,
+    )
+    .await?;
+    Ok(HttpResponse::Ok().json(format!("{{ uri: {} }}", uri)))
 }
