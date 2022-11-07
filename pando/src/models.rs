17c0db11cc1bbe6801e5c1db45c435f33ac46670
jiito <benjamin.allanrahill@gmail.com>
2022-11-07 16:58:18 -0500

    [asp-50]: feat: create project handler

--- pando/src/models.rs
+++ pando/src/models.rs
@@ -1,7 +1,8 @@
 use crate::schema::*;
 use diesel::prelude::*;
+use serde::Serialize;
 
-#[derive(Queryable, Identifiable)]
+#[derive(Queryable, Identifiable, Debug, Serialize)]
 pub struct Project {
     pub id: i32,
     pub name: String,
@@ -13,12 +14,19 @@ pub struct NewProject<'a> {
     pub name: &'a str,
 }
 
-#[derive(Queryable, Associations, Identifiable)]
+#[derive(Queryable, Associations, Identifiable, Debug)]
 #[belongs_to(Project)]
 #[table_name = "developers"]
 pub struct Developer {
     pub id: i32,
     pub name: String,
-    pub ip_address: String,
+    pub ip_address: ipnetwork::IpNetwork,
+    pub project_id: Option<i32>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = developers)]
+pub struct NewDeveloper {
+    pub name: String,
     pub project_id: i32,
 }
--- pando/src/storage.rs
+++ pando/src/storage.rs
@@ -1,3 +1,4 @@
+pub mod db;
 pub mod file;
 pub mod gcs;
 pub mod pando;
