af410ebca05133c654ba966107b8e037e4a013a9
jiito <benjamin.allanrahill@gmail.com>
2022-11-09 13:00:19 -0500

    [asp-61]: feat: add host sub commands

--- pando/src/bin/main.rs
+++ pando/src/bin/main.rs
@@ -8,8 +8,8 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Host {
-        #[arg(short, long)]
-        host: bool,
+        #[command(subcommand)]
+        command: Option<HostCommands>,
     },
     Developer {
         #[command(subcommand)]
@@ -24,15 +24,22 @@ enum DeveloperCommands {
         files: std::path::PathBuf,
     },
 }
+#[derive(Subcommand, Debug)]
+enum HostCommands {
+    Start {},
+}
 fn main() {
     let cli = Args::parse();
 
     match &cli.command {
-        Some(Commands::Host { host }) => {
-            if *host {
-                println!("You are hosting something")
+        Some(Commands::Host { command }) => match command {
+            Some(HostCommands::Start {}) => {
+                println!("Starting the ASPN cloud")
             }
-        }
+            None => {
+                println!("No files provided!")
+            }
+        },
         Some(Commands::Developer { command }) => match command {
             Some(DeveloperCommands::Upload { files }) => {
                 println!("{}", files.to_str().expect("Not a valid Path"))
