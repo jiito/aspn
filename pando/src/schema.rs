90980cdc0902203b02d50918df44046285aed04f
jiito <benjamin.allanrahill@gmail.com>
2022-11-21 14:31:41 -0500

    [asp-84]: migration run

--- pando/src/schema.rs
+++ pando/src/schema.rs
@@ -19,14 +19,34 @@ diesel::table! {
 }
 
 diesel::table! {
+    hosts (id) {
+        id -> Int4,
+        ip_address -> Inet,
+        user_token -> Varchar,
+    }
+}
+
+diesel::table! {
+    hosts_functions (function_id, host_id) {
+        function_id -> Int4,
+        host_id -> Int4,
+    }
+}
+
+diesel::table! {
     projects (id) {
         id -> Int4,
         name -> Varchar,
     }
 }
 
+diesel::joinable!(hosts_functions -> functions (function_id));
+diesel::joinable!(hosts_functions -> hosts (host_id));
+
 diesel::allow_tables_to_appear_in_same_query!(
     developers,
     functions,
+    hosts,
+    hosts_functions,
     projects,
 );
