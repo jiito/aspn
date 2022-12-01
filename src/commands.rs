2b04f80f8d147a028a6835bc201a73f28d2695ce
jiito <benjamin.allanrahill@gmail.com>
2022-12-01 15:23:36 -0500

    [asp-94]: add auth back from pando

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,12 @@
 pub mod developer;
 pub mod host;
-use crate::{config, utils};
+use crate::{
+    config,
+    utils::{
+        self,
+        api::{self, models},
+    },
+};
 use clap::Subcommand;
 use local_ip_address::local_ip;
 use webbrowser;
@@ -28,30 +34,26 @@ pub fn init() {
 }
 
 pub async fn auth() {
-    todo!();
-    // let device_code = utils::auth::request_device_code()
-    //     .await
-    //     .expect("Could not get device code");
+    let device_code = utils::auth::request_device_code()
+        .await
+        .expect("Could not get device code");
 
-    // if webbrowser::open(&device_code.verification_uri_complete).is_ok() {
-    //     let access_token =
-    //         utils::auth::request_access_token(device_code.device_code, device_code.interval)
-    //             .await
-    //             .expect("Could not get access token");
-    //     let user = utils::auth::get_user(&access_token.access_token)
-    //         .await
-    //         .expect("Could not get user");
-    //     println!("Successfully logged in!  Hello, {}!!", user.name);
+    if webbrowser::open(&device_code.verification_uri_complete).is_ok() {
+        let access_token =
+            utils::auth::request_access_token(device_code.device_code, device_code.interval)
+                .await
+                .expect("Could not get access token");
+        let user = utils::auth::get_user(&access_token.access_token)
+            .await
+            .expect("Could not get user");
+        println!("Successfully logged in!  Hello, {}!!", user.name);
 
-    //     // TODO: find way to branch this before the host save
-    //     utils::config::host::save_token_to_config(&access_token.access_token);
-    //     let conn = &mut establish_connection();
-    //     let host = models::NewHost {
-    //         ip_address: ipnetwork::IpNetwork::new(local_ip().unwrap(), 32).unwrap(),
-    //         user_token: access_token.access_token,
-    //     };
-    //     host.save(conn)
-    // }
-
-    // Connect to a project
+        // TODO: find way to branch this before the host save
+        config::host::save_token_to_config(&access_token.access_token);
+        let host = models::NewHost {
+            ip_address: ipnetwork::IpNetwork::new(local_ip().unwrap(), 32).unwrap(),
+            user_token: access_token.access_token,
+        };
+        api::storage::host::save(host).await;
+    }
 }
