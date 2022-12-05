3ae575e6b785c303b83c4d7450179864298fee4c
jiito <benjamin.allanrahill@gmail.com>
2022-12-05 12:56:10 -0500

    [main]: i/o types

--- src/utils/wasm.rs
+++ src/utils/wasm.rs
@@ -1,6 +1,21 @@
+use std::io::Read;
+use wasi_common::pipe::{ReadPipe, WritePipe};
+
 use anyhow::{Context, Result};
+use serde::{Deserialize, Serialize};
 use wasmtime::*;
 use wasmtime_wasi::{sync::WasiCtxBuilder, WasiCtx};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Input {
+    pub name: String,
+    pub num: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Output {
+    pub names: Vec<String>,
+}
 
 pub fn run(path: &str) -> Result<()> {
     // Define the WASI functions globally on the `Config`.
@@ -11,24 +26,48 @@ pub fn run(path: &str) -> Result<()> {
     // Create a WASI context and put it in a Store; all instances in the store
     // share this context. `WasiCtxBuilder` provides a number of ways to
     // configure what the target program will have access to.
+
+    // TAKEN FROM https://github.com/pmalmgren/wasi-data-sharing/blob/shared-stdio-demo/examples/wasi/main.rs
+    let input = Input {
+        name: "Rust".into(),
+        num: 10,
+    };
+    let serialized_input = serde_json::to_string(&input)?;
+
+    let stdin = ReadPipe::from(serialized_input);
+    let stdout = WritePipe::new_in_memory();
+
     let wasi = WasiCtxBuilder::new()
-        .inherit_stdio()
-        .inherit_args()?
+        .stdin(Box::new(stdin.clone()))
+        .stdout(Box::new(stdout.clone()))
         .build();
+    let module = Module::from_file(&engine, path)?;
+
     let mut store = Store::new(&engine, wasi);
 
-    // Instantiate our module with the imports we've created, and run it.
-    let module = Module::from_file(&engine, path)?;
-    linker.module(&mut store, "", &module)?;
-    let ext = linker
+    linker
+        .module(&mut store, "", &module)
+        .expect("linking the function");
+    linker
         .get(&mut store, "", "greet")
-        .expect("Can't find extern")
-        .into_func();
+        .expect("should get the wasi runtime")
+        .into_func()
+        .expect("Couldnt'turn into func")
+        .typed::<(), (), _>(&store)
+        .expect("should type the function")
+        .call(&mut store, ())
+        .expect("should call the function");
 
-    match ext {
-        Some(f) => f.typed::<(), (), _>(&store)?.call(&mut store, ())?,
-        None => {}
-    }
+    drop(store);
+
+    let contents: Vec<u8> = stdout
+        .try_into_inner()
+        .map_err(|_err| anyhow::Error::msg("sole remaining reference"))?
+        .into_inner();
+    let output: Output = serde_json::from_slice(&contents)?;
+
+    println!("outputnames: {:?}", output.names);
+
     Ok(())
 }
 #[cfg(test)]
