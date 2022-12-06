260c810e95fa27907a65ce7bc4362355f13a17f4
jiito <benjamin.allanrahill@gmail.com>
2022-12-06 07:39:56 -0500

    [main]: initial env var setup

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,16 @@
 pub mod dev;
 pub mod host;
 pub mod project;
+
+pub mod env {
+
+    pub fn database_url() -> String {
+        std::env::var("DATABASE_URL").unwrap_or("postgresql://localhost:5432/pando".to_string())
+    }
+    pub fn api_url() -> String {
+        std::env::var("API_URL").unwrap_or("localhost:8080".to_string())
+    }
+    pub fn degub() -> bool {
+        api_url().eq("localhost:8080")
+    }
+}
