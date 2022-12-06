4451f79a06eed975981454fdcc32ed56b686daed
jiito <benjamin.allanrahill@gmail.com>
2022-12-06 07:40:25 -0500

    [main]: fix: typo

--- src/config.rs
+++ src/config.rs
@@ -10,7 +10,7 @@ pub mod env {
     pub fn api_url() -> String {
         std::env::var("API_URL").unwrap_or("localhost:8080".to_string())
     }
-    pub fn degub() -> bool {
+    pub fn debug() -> bool {
         api_url().eq("localhost:8080")
     }
 }
