5890cb16c949a1f381adc2554368aba7d9219fa4
jiito <benjamin.allanrahill@gmail.com>
2022-12-06 07:45:17 -0500

    [main]: removing db url

--- src/config.rs
+++ src/config.rs
@@ -4,9 +4,6 @@ pub mod project;
 
 pub mod env {
 
-    pub fn database_url() -> String {
-        std::env::var("DATABASE_URL").unwrap_or("postgresql://localhost:5432/pando".to_string())
-    }
     pub fn api_url() -> String {
         std::env::var("API_URL").unwrap_or("localhost:8080".to_string())
     }
