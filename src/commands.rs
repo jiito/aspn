3f991c51be9f5059e62f1adbfef60b2134fd87ef
jiito <benjamin.allanrahill@gmail.com>
2022-12-06 21:13:04 -0500

    [main]: adding spinner

--- src/commands.rs
+++ src/commands.rs
@@ -9,6 +9,7 @@ use crate::{
         self,
         api::{self, models},
     },
+    with_loader,
 };
 use anyhow::Result;
 use clap::Subcommand;
@@ -52,15 +53,24 @@ pub enum UserType {
 }
 
 pub async fn auth(user_type: UserType) -> Result<()> {
+    if is_authenticated() {
+        let host_creds = config::host::read_config().host.expect("No credentials");
+        let user = utils::auth::get_user(&host_creds.token)
+            .await
+            .expect("Could not get user");
+        println!("You're already logged in as {}", user.email);
+        return Ok(());
+    }
     let device_code = utils::auth::request_device_code()
         .await
         .expect("Could not get device code");
 
+    println!("Opening {}", &device_code.verification_uri_complete.clone());
     if webbrowser::open(&device_code.verification_uri_complete).is_ok() {
-        let access_token =
-            utils::auth::request_access_token(device_code.device_code, device_code.interval)
-                .await
-                .expect("Could not get access token");
+        let access_token = with_loader!(
+            utils::auth::request_access_token(device_code.device_code, device_code.interval).await
+        )
+        .expect("Could not get access token");
         let user = utils::auth::get_user(&access_token.access_token)
             .await
             .expect("Could not get user");
@@ -87,4 +97,12 @@ pub async fn auth(user_type: UserType) -> Result<()> {
         }
     };
     Ok(())
+}
+
+fn is_authenticated() -> bool {
+    let config = config::host::read_config();
+    match config.host {
+        Some(_) => true,
+        None => false,
+    }
 }
