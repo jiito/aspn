65e96545de536810250012889a521377873e6653
jiito <benjamin.allanrahill@gmail.com>
2022-12-07 16:29:56 -0500

    [main]: feat: dummy proxy

--- src/http/server.rs
+++ src/http/server.rs
@@ -7,13 +7,13 @@ use crate::utils;
 
 pub async fn start() {
     // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 4011));
 
     // A `Service` is needed for every connection, so this
     // creates one from our `hello_world` function.
     let make_svc = make_service_fn(|_conn| async {
         // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(handler))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
@@ -23,10 +23,18 @@ pub async fn start() {
         eprintln!("server error: {}", e);
     }
 }
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let wasm_res =
-        utils::wasm::run("/Users/bjar/git/aspn/examples/wasm/target/wasm32-wasi/debug/wasm.wasm")
-            .unwrap();
-    let json_str = serde_json::to_string(&wasm_res).unwrap();
-    Ok(Response::new(json_str.into()))
+async fn handler(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("[PROXY]: {:?}", _req.body());
+    let route = String::from("/wasm-hello-world");
+    let uri = _req.uri();
+    if uri.path().eq(&route) {
+        let wasm_res = utils::wasm::run(
+            "/Users/bjar/git/aspn/examples/wasm/target/wasm32-wasi/debug/wasm.wasm",
+        )
+        .unwrap();
+        let json_str = serde_json::to_string(&wasm_res).unwrap();
+        Ok(Response::new(json_str.into()))
+    } else {
+        Ok(Response::new("".into()))
+    }
 }
