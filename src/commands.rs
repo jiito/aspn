125b4d1be9d795ff174cdc30c7efafb9a682336a
jiito <benjamin.allanrahill@gmail.com>
2022-12-11 22:48:44 -0500

    [main]: fix: login races

--- src/commands.rs
+++ src/commands.rs
@@ -11,7 +11,7 @@ use crate::{
     },
     with_loader,
 };
-use anyhow::Result;
+use anyhow::{Context, Result};
 use clap::Subcommand;
 use local_ip_address::local_ip;
 use webbrowser;
@@ -34,9 +34,13 @@ pub async fn init() -> Result<()> {
         Ok(name) => {
             let mut config = config::dev::create_config(&name);
 
-            let developer = api::storage::developer::current().await?;
+            let developer = api::storage::developer::current()
+                .await
+                .context("Can't find developer")?;
 
-            let project = api::storage::project::save(&name, &developer.id).await?;
+            let project = api::storage::project::save(&name, &developer.id)
+                .await
+                .context("Can't save project")?;
 
             config.project.id = Some(project.id);
             config::dev::write(&config);
@@ -58,44 +62,31 @@ pub async fn auth(user_type: UserType) -> Result<()> {
         let user = utils::auth::get_user(&host_creds.token)
             .await
             .expect("Could not get user");
+        save_user(user_type, &host_creds.token).await?;
         println!("You're already logged in as {}", user.email);
-        return Ok(());
-    }
-    let device_code = utils::auth::request_device_code()
-        .await
-        .expect("Could not get device code");
-
-    println!("Opening {}", &device_code.verification_uri_complete.clone());
-    if webbrowser::open(&device_code.verification_uri_complete).is_ok() {
-        let access_token = with_loader!(
-            utils::auth::request_access_token(device_code.device_code, device_code.interval).await
-        )
-        .expect("Could not get access token");
-        let user = utils::auth::get_user(&access_token.access_token)
+    } else {
+        let device_code = utils::auth::request_device_code()
             .await
-            .expect("Could not get user");
+            .expect("Could not get device code");
 
-        // TODO: find way to branch this before the host save
-        config::host::save_token_to_config(&access_token.access_token);
+        println!("Opening {}", &device_code.verification_uri_complete.clone());
+        if webbrowser::open(&device_code.verification_uri_complete).is_ok() {
+            let access_token = with_loader!(
+                utils::auth::request_access_token(device_code.device_code, device_code.interval)
+                    .await,
+                "Authenticating"
+            )
+            .expect("Could not get access token");
+            let user = utils::auth::get_user(&access_token.access_token)
+                .await
+                .expect("Could not get user");
 
-        match user_type {
-            UserType::Dev => {
-                let dev = models::NewDeveloper {
-                    name: user.name,
-                    project_id: None,
-                    auth_token: Some(access_token.access_token.clone()),
-                };
-                api::storage::developer::save(&dev).await?;
-            }
-            UserType::Host => {
-                let host = models::NewHost {
-                    ip_address: ipnetwork::IpNetwork::new(local_ip()?, 32)?,
-                    user_token: access_token.access_token,
-                };
-                api::storage::host::save(host).await?;
-            }
-        }
-    };
+            // TODO: find way to branch this before the host save
+            config::host::save_token_to_config(&access_token.access_token);
+            save_user(user_type, &access_token.access_token).await?;
+            println!("Successfully logged in as: {}", user.email);
+        };
+    }
     Ok(())
 }
 
@@ -105,4 +96,23 @@ fn is_authenticated() -> bool {
         Some(_) => true,
         None => false,
     }
+}
+async fn save_user(user_type: UserType, token: &String) -> Result<()> {
+    match user_type {
+        UserType::Dev => {
+            let dev = models::NewDeveloper {
+                project_id: None,
+                auth_token: Some(token.clone()),
+            };
+            api::storage::developer::save(&dev).await?;
+        }
+        UserType::Host => {
+            let host = models::NewHost {
+                ip_address: ipnetwork::IpNetwork::new(local_ip()?, 32)?,
+                user_token: token.clone(),
+            };
+            api::storage::host::save(host).await?;
+        }
+    }
+    Ok(())
 }
